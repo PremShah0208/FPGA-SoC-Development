29a66225a572cc18fdc215e6a3f5669e12008137
Prem Shah <82882435+PremShah0208@users.noreply.github.com>
2026-08-10 15:24:38 -0700

    Correct triple voter RTL implementation

--- src/triple_voter.sv
+++ src/triple_voter.sv
@@ -1,12 +1,17 @@
 module triple_voter(
+    input  logic clk,
+    input  logic rst_n,
     input  logic a,
     input  logic b,
     input  logic c,
     output logic y
 );
 
-    always_comb begin
-        y = (a & b) | (a & c) | (b & c);
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n)
+            y <= 1'b0;
+        else
+            y <= (a & b) | (a & c) | (b & c);
     end
 
 endmodule
