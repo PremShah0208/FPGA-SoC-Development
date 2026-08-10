4649572ea832c4ea549b31b00057de05b52fc8e3
Prem Shah <82882435+PremShah0208@users.noreply.github.com>
2026-08-10 16:33:31 -0700

    Update triple voter testbench with assertion-based verification

--- tb/triple_voter_tb.sv
+++ tb/triple_voter_tb.sv
@@ -1,4 +1,9 @@
+`timescale 1ns / 1ps
+
 module triple_voter_tb;
+
+    timeunit 1ns/100ps;
+    localparam time CP = 10ns;
 
     logic clk;
     logic rst_n;
@@ -16,28 +21,93 @@ module triple_voter_tb;
         .y     (y)
     );
 
-    initial begin
-        clk = 0;
-        forever #5 clk = ~clk;
+    function automatic tmr_calc(input a, input b, input c);
+        begin
+            return (a & b) | (b & c) | (a & c);
+        end
+    endfunction
+
+    always begin
+        clk = 1'b1;
+        #(CP/2);
+        clk = 1'b0;
+        #(CP/2);
     end
 
     initial begin
-        rst_n = 0;
-        a = 0;
-        b = 0;
-        c = 0;
+        $timeformat(-9, 0, "ns");
+        $monitor(
+            "Time %0t: a = %0b, b = %0b, c = %0b, y = %0b",
+            $time, a, b, c, y
+        );
 
-        #12;
-        rst_n = 1;
+        clk   = 1'b0;
+        rst_n = 1'b0;
+        a     = 1'b0;
+        b     = 1'b0;
+        c     = 1'b0;
 
-        {a, b, c} = 3'b000; #10;
-        {a, b, c} = 3'b001; #10;
-        {a, b, c} = 3'b010; #10;
-        {a, b, c} = 3'b011; #10;
-        {a, b, c} = 3'b100; #10;
-        {a, b, c} = 3'b101; #10;
-        {a, b, c} = 3'b110; #10;
-        {a, b, c} = 3'b111; #10;
+        #10;
+        rst_n = 1'b1;
+        #10;
+
+        $display("----------------");
+        $display("Test Start");
+        $display("----------------");
+
+        wait (rst_n == 1'b1);
+
+        @(posedge clk);
+        a = 1'b0;
+        b = 1'b0;
+        c = 1'b0;
+
+        @(posedge clk);
+        assert (y == tmr_calc(a, b, c))
+            else $error("Triple voter mismatch");
+
+        c = 1'b1;
+        @(posedge clk);
+        assert (y == tmr_calc(a, b, c))
+            else $error("Triple voter mismatch");
+
+        b = 1'b1;
+        c = 1'b0;
+        @(posedge clk);
+        assert (y == tmr_calc(a, b, c))
+            else $error("Triple voter mismatch");
+
+        c = 1'b1;
+        @(posedge clk);
+        assert (y == tmr_calc(a, b, c))
+            else $error("Triple voter mismatch");
+
+        a = 1'b1;
+        b = 1'b0;
+        c = 1'b0;
+        @(posedge clk);
+        assert (y == tmr_calc(a, b, c))
+            else $error("Triple voter mismatch");
+
+        c = 1'b1;
+        @(posedge clk);
+        assert (y == tmr_calc(a, b, c))
+            else $error("Triple voter mismatch");
+
+        b = 1'b1;
+        c = 1'b0;
+        @(posedge clk);
+        assert (y == tmr_calc(a, b, c))
+            else $error("Triple voter mismatch");
+
+        c = 1'b1;
+        @(posedge clk);
+        assert (y == tmr_calc(a, b, c))
+            else $error("Triple voter mismatch");
+
+        $display("----------------");
+        $display("Test end");
+        $display("----------------");
 
         $finish;
     end
